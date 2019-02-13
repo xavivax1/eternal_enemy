40b7b05ba170bd990416c939653ef81bbc2a55ff
xavivax1 <xavier.deviala@gmail.com>
2019-02-13 14:05:40 +0100

    first

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 'use strict';
+const main=() => {
+    const buildDom = (html) => {
+      
+        const main =document.querySelector('main');
+        main.innerHTML = html;
+        return main;
+
+    };
+    const buildSplashScreen = () => {
+       const splashScreen = buildDom(`
+       <section class="splash-screen">
+       <h1>Eternal enemies</h1>
+       <button>Start</button>
+       </section>
+       <h1>Hello</h1>
+       `);
+    };
+    buildSplashScreen();
+
+}
+
+
+window.addEventListener('load',main);
